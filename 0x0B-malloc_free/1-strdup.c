0d784ce5a014fcc2d323f8b7a2e7ee3611544804
DesignSteve <steveboss008@gmail.com>
2023-08-08 16:13:29 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,38 +1,33 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-* str_concat - a function that concatenates two strings.
-*@s1:First string
-*@s2:Second string
-*
-*Return: NULL in case of failure , but pointer to new string in
-*case of success
-*/
-
-char *str_concat(char *s1, char *s2)
+ * _strdup - function that returns a pointer to a newly allocated space in
+ *           memory, which contains a copy of the string given as a parameter.
+ *
+ * @str: This is the input string
+ *
+ * Return: function returns a pointer to the duplicated string. It returns 
+ *         NULL if insufficient memory was available
+ */
+char *_strdup(char *str)
 {
-	char *concat_str;
-	int index, concat_index = 0,  len = 0;
+	char *s;
+	int a, b;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	s = (char *)  malloc((a + 1) * sizeof(char));
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+	if (s == NULL)
+		return (NULL);
 
-	return (concat_str);
+	for (b = 0; b < a ; b++)
+		s[b] = str[b];
+
+	s[a + 1] = '\0';
+	return (s);
 }
