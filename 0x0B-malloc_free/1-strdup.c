3ecba788b3ed11df7d1f310f8a8f606947127b7b
DesignSteve <steveboss008@gmail.com>
2023-08-08 16:11:20 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,29 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
- */
-char *_strdup(char *str)
+* str_concat - a function that concatenates two strings.
+*@s1:First string
+*@s2:Second string
+*
+*Return: NULL in case of failure , but pointer to new string in
+*case of success
+*/
+
+char *str_concat(char *s1, char *s2)
 {
-	char *aaa;
-	int i, r = 0;
+	char *concat_str;
+	int index, concat_index = 0,  len = 0;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
 
-	aaa = malloc(sizeof(char) * (i + 1));
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
-	if (aaa == NULL)
-		return (NULL);
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-
-	return (aaa);
+	return (concat_str);
 }
