e47c7b04ac0caa62595316857c1032180ded009e
DesignSteve <steveboss008@gmail.com>
2023-10-30 16:03:13 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,55 +6,41 @@
 #include <fcntl.h>
 
 /**
- * create_file - Create a file and write contents to it
- * @filename: Pointer to the file name
- * @text_content: Pointer to the text content
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
  *
- * Description: This function creates a file and writes text content to it.
- *
- * Return: 1 on success, -1 on failure
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int count = 0;
-    int fd = 0;
-    int output = 0;
+	int count = 0;
+	int fd = 0;
+	int output = 0;
 
-    // Check if the filename is valid
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    // Open the file for writing, creating it if it doesn't exist
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    // If text_content is not NULL, determine its length
-    if (text_content != NULL)
-    {
-        while (text_content[count] != '\0')
-            count++;
-    }
-    else
-    {
-        // If text_content is NULL, close the file and return success
-        close(fd);
-        return (1);
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+	{
+		close(fd);
+		return (1);
+	}
 
-    // Write the text content to the file
-    output = write(fd, text_content, count);
+	output = write(fd, text_content, count);
+	if (output == -1 || output != count)
+		return (-1);
 
-    // Check for write errors or incomplete writes
-    if (output == -1 || output != count)
-    {
-        close(fd);
-        return (-1);
-    }
-
-    // Close the file
-    close(fd);
-
-    return (1);
+	close(fd);
+	return (1);
 }
-
