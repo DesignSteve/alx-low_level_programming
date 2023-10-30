296869114af3d97baf83358cdd8d1622439fc9c3
DesignSteve <steveboss008@gmail.com>
2023-10-30 15:55:17 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,32 @@
 #include "main.h"
-
 /**
- * create_file - creates a file and writes a NULL-terminated string to it
+ * create_file -creates an array of chars, and initializes
  *
- * @filename: the name of the file to be created
- * @text_content: the string to write to the file
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, bytes_written, length = 0;
+	int o, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (length = 0; text_content[length]; length++)
-            ; // Calculate the length of the text_content
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    bytes_written = write(file_descriptor, text_content, length);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-    if (file_descriptor == -1 || bytes_written == -1)
-        return (-1);
+	if (o == -1 || w == -1)
+		return (-1);
 
-    close(file_descriptor);
+	close(o);
 
-    return (1);
+	return (1);
 }
-
